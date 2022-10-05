0f5e6d8c2b2e7d3bc3bf9638f0439de3ffcb563a
jessica-millenia <jessica.millenia@rey.id>
2022-10-05 18:07:44 +0700

    grpc boilerplate code

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/jessica-millenia/go-grpc/greet/proto"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var address string = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", address)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", address)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
